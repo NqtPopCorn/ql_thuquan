759d456eaadb04307f57a7aa70e6bfd1ce7a27a0
truong <truong263111@gmail.com>
2025-03-29 16:54:14 +0700

    tao dashboard

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            cardPanel = new Panel();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 1);
+            tableLayoutPanel1.Controls.Add(button2, 0, 2);
+            tableLayoutPanel1.Controls.Add(button3, 0, 3);
+            tableLayoutPanel1.Controls.Add(button4, 0, 4);
+            tableLayoutPanel1.Controls.Add(button5, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Left;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 200F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 80F));
+            tableLayoutPanel1.Size = new Size(250, 800);
+            tableLayoutPanel1.TabIndex = 0;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Fill;
+            button1.Location = new Point(3, 203);
+            button1.Name = "button1";
+            button1.Size = new Size(244, 167);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Fill;
+            button2.Location = new Point(3, 376);
+            button2.Name = "button2";
+            button2.Size = new Size(244, 167);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Dock = DockStyle.Fill;
+            button3.Location = new Point(3, 549);
+            button3.Name = "button3";
+            button3.Size = new Size(244, 167);
+            button3.TabIndex = 2;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Dock = DockStyle.Fill;
+            button4.Location = new Point(3, 722);
+            button4.Name = "button4";
+            button4.Size = new Size(244, 75);
+            button4.TabIndex = 3;
+            button4.Text = "btnDangXuat";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Dock = DockStyle.Fill;
+            button5.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button5.Location = new Point(3, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(244, 194);
+            button5.TabIndex = 4;
+            button5.Text = "Info";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // cardPanel
+            // 
+            cardPanel.Dock = DockStyle.Fill;
+            cardPanel.Location = new Point(250, 0);
+            cardPanel.Name = "cardPanel";
+            cardPanel.Size = new Size(1100, 800);
+            cardPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1350, 800);
+            Controls.Add(cardPanel);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Panel cardPanel;
+        private Button button5;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,73 @@
+using System.Windows.Forms;
+
 namespace QL_ThuQuan
 {
     public partial class Form1 : Form
     {
+
+        private Dictionary<Button, UserControl> navMap;
+        private Button currentSelectedButton;
+
         public Form1()
         {
             InitializeComponent();
+
+            SetUpNavigations();
+            
+        }
+
+        private void ShowControl(UserControl control)
+        {
+            control.Dock = DockStyle.Fill;
+            cardPanel.Controls.Clear(); 
+            cardPanel.Controls.Add(control); 
+        }
+
+        private void SetUpNavigations()
+        {
+            navMap = new Dictionary<Button, UserControl>() {
+                { button1, new UserControl1() },
+                { button2, new UserControl2() },
+            };
+
+            foreach (var entry in navMap)
+            {
+                Button btn = entry.Key;
+                UserControl ctrl = entry.Value;
+
+                btn.Click += (s, e) =>
+                {
+                    ShowControl(ctrl);
+                    SetSelectedButton(btn);
+                };
+            }
+        }
+
+        private void SetSelectedButton(Button selectedBtn)
+        {
+            foreach (var btn in navMap.Keys)
+            {
+                btn.BackColor = SystemColors.Control; 
+            }
+
+            selectedBtn.BackColor = Color.LightBlue;
+            currentSelectedButton = selectedBtn;
+        }
+
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
